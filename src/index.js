8a954228f873d47e3b4bfb7a3754ea8896d98d08
Alla Borisova <alla.borisova27@gmail.com>
2022-02-05 11:40:35 +0500

    something do

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  let arr = matrix.map((element, index) => {
+    if (index % 2 !== 0) {
+      return element.reverse();
+    } else {
+      return element;
+    }
+  })
+  let str = arr.join(',');
+  let newarr = str.split(',').map(Number);
+  return newarr;
 }
